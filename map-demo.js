baab3c8e22d15f3e37ec77971924c50a438e1f80
j1w0os <102014705+sonjiwoo1215@users.noreply.github.com>
2025-02-18 20:42:28 +0900

    0218

---
+++ map-demo.js
@@ -0,0 +1,9 @@
+const arr = [1,2,3,4,5]
+
+arr.forEach(function(a,b,c){
+    console.log(`a : ${a}, b : ${b}, c : ${c}`)
+})
+
+arr.map(function(a,b,c){
+    console.log(`a : ${a}, b : ${b}, c : ${c}`)
+})
--- youtuber-demo.js
+++ youtuber-demo.js
@@ -29,8 +29,15 @@ db.set(id++, youtuber1)
 db.set(id++, youtuber2)
 db.set(id++, youtuber3)
 
+app.get('/youtubers', (req,res)=>{
+    var youtubers = {}
+    db.forEach(function(value,key){
+        youtubers[key] = value
+    });
+    res.json(youtubers)
+})
 
-app.get('/youtuber/:id',function(req,res){
+app.get('/youtubers/:id',function(req,res){
     let {id} = req.params
     // console.log(id)
     id = parseInt(id)
@@ -47,13 +54,7 @@ app.get('/youtuber/:id',function(req,res){
 
 app.use(express.json())
 
-app.get('/youtubers', (req,res)=>{
-    res.json({
-        message : "test"
-    })
-})
-
-app.post('/youtuber', (req,res)=>{
+app.post('/youtubers', (req,res)=>{
     console.log(req.body)
 
     db.set(id++, req.body)
@@ -61,4 +62,60 @@ app.post('/youtuber', (req,res)=>{
     res.json({
         message: `${db.get(id-1).channelTitle}님, 유튜버 생활을 응원합니다.`
     })
+})
+
+app.delete('/youtubers/:id', (req,res)=>{
+    let {id} = req.params
+    id = parseInt(id)
+
+    var youtuber = db.get(id); 
+    if(youtuber == undefined){
+        res.json({
+            message : `요청하신 ${id}번은 없는 유튜버다` 
+        })
+    }else{
+        const channelTitle = youtuber.channelTitle
+        db.delete(id)
+    
+        res.json({
+            message : `${channelTitle}님, 잘가요`
+        })
+    }
+})
+
+app.delete('/youtubers', (req,res)=>{
+
+    var msg = ''
+    if(db.size >= 1){
+        db.clear()
+        msg = "전체 유튜버 삭제"
+    }else{
+        msg = "삭제할 유튜버 없음"
+    }
+    res.json({
+        message : msg
+    })
+
+})
+
+app.put('/youtubers/:id', (req,res)=>{
+    let {id} = req.params
+    id = parseInt(id)
+
+    var youtuber = db.get(id); 
+    var oldTitle = youtuber.channelTitle
+    if(youtuber == undefined){
+        res.json({
+            message : `요청하신 ${id}번은 없는 유튜버다` 
+        })
+    }else{
+        var newTitle = req.body.channelTitle
+
+        youtuber.channelTitle = newTitle
+        db.set(id,youtuber)
+
+        res.json({
+            message : `${oldTitle}님, 채널명이 ${newTitle}로 수정되었습니다.` 
+        })
+    }
 })
